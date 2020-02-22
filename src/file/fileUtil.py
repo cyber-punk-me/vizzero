f1bf28dc561b094847f8dc5f1f5ed6114cc4f41f
Kirill <ekirill.mm@gmail.com>
2020-02-22 15:24:18 -0500

    Save files with gesture names

--- src/file/fileUtil.py
+++ src/file/fileUtil.py
@@ -6,9 +6,10 @@ from pathlib import Path
 
 
 class FileWriter:
-    def __init__(self, path_to_file="../data/"):
+    def __init__(self, path_to_file="../data/", gesture_label=None):
         self._path = path_to_file
         self.f = None
+        self.gesture_label = gesture_label
 
     def start_file(self):
         try:
@@ -16,12 +17,15 @@ class FileWriter:
         except:
             pass
 
-        filename = str(datetime.datetime.today().strftime("%Y-%m-%d_%H_%M_%S")) + ".csv"
+        if self.gesture_label is None:
+            filename = str(datetime.datetime.today().strftime("%Y-%m-%d_%H_%M_%S")) + ".csv"
+        else:
+            filename = self.gesture_label._name_ + '.csv'
         path_file = Path(self._path) / filename
         self.f = open(path_file, 'ab+')
 
     def append_data(self, data):
-        np.savetxt(self.f, data, delimiter=',', newline=',')
+        np.savetxt(self.f, data, delimiter=',')
 
     def finish_file(self):
         # remove last comma and close file (maybe comma will be need for target label in the future)
@@ -33,3 +37,4 @@ class FileWriter:
         files = sorted(glob.glob(self._path + "*.csv"))
         if len(files) != 0:
             os.remove(files[-1])
+
